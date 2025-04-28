c95d427dcfd0ffb6f031fdfcec43bca540782646
TONY STAR 002 <antonyyomar202403@gmail.com>
2025-04-27 23:19:42 -0500

    Delete SEMANA 4 directory

--- SEMANA 4/ejercicio_cadena.py
+++
@@ -1,9 +0,0 @@
-cadena = input("Ingrese una cadena de texto : ")
-palabras = cadena.split()
-recuento = dict()
-
-print("Palabras: ", palabras)
-
-for palabra in palabras:
-    recuento[palabra] = recuento.get(palabra, 0) + 1
-print("Recuento: ", recuento)
